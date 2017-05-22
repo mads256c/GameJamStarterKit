b1f222342fc1b5705dfa06ebb25d802b882ffb01
julio-hb <julius@juto.dk>
2017-05-22 21:08:21 +0200

    Game manager

---
+++ Assets/GameJamStarterKit/GlobalScripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static bool isPaused
+    {
+        get
+        {
+            return ispaused;
+        }
+        set
+        {
+            ispaused = value;
+
+            OnPause();
+        }
+    }
+
+    private static bool ispaused = false;
+
+    private static void OnPause()
+    {
+        Debug.Log("isPaused is now: " + ispaused);
+    }
+
+}
