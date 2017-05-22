d05944b9fc45d002be9b2e02bb526cd4bb257552
julio-hb <julius@juto.dk>
2017-05-22 22:19:48 +0200

    Added enemy
    Enemy added, not tested tho.

---
+++ Assets/GameJamStarterKit/SideScroller2D/Scripts/Enemy/Interface/IEnemyState.cs
@@ -0,0 +1,20 @@
+﻿/**
+ * Author: Julius Bendt
+ * Author URI: www.juto.dk
+ * Company: JutoGames
+ * Company URI: www.juto.dk
+ * Copyright 2015
+ **/
+
+using UnityEngine;
+
+namespace GameJamStarterKit.Sidescroller
+{
+    public interface IEnemyState
+    {
+        void Execute();
+        void Enter(Enemy enemy);
+        void Exit();
+        void OnTriggerEnter(Collider2D o);
+    }
+}
