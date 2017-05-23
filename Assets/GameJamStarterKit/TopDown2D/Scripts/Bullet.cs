9840daf0bfbd1349c1d363e9d645ab0b2ce8bfaf
Mads Mogensen <lort256c@gmail.com>
2017-05-23 16:22:36 +0200

    Added simple bullet projectile script

---
+++ Assets/GameJamStarterKit/TopDown2D/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class Bullet : MonoBehaviour {
+
+    [Tooltip("How fast should the bullet be?")]
+    public float Speed = 20f;
+    [Tooltip("How long the bullet should be in the world before it disappears.")]
+    public float TimeToLife = 10f;
+    [Tooltip("What should the bullet damage?")]
+    public LayerMask DamageLayer;
+
+    private new Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rigidbody.velocity = transform.right * Speed * Time.deltaTime;
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == DamageLayer)
+        {
+            //Take dammage
+        }
+    }
+}
