2ed795d234e03de96c0bde2ef03c61e12a452935
Mads Mogensen <lort256c@gmail.com>
2017-08-17 20:22:43 +0200

    Added TestScripts
    +AudioTest.cs
    +Audio/Ding.mp3

---
+++ Assets/TestScripts/AudioTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using GameJamStarterKit;
+
+namespace TestScripts
+{
+
+    public class AudioTest : MonoBehaviour
+    {
+        public AudioClip audioClip;
+
+        private void Update()
+        {
+            if (Input.anyKeyDown)
+            AudioManager.PlaySound(new Vector3(0, 0, 0), audioClip);
+        }
+    }
+}
