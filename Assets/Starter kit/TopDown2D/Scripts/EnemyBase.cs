77446f161cc5a13b97bab79a0ac9336ff6107cb3
Mads Mogensen <lort256c@gmail.com>
2017-08-19 00:34:18 +0200

    Replaced Enemy AI with a more powerfull Baseclass

---
+++ Assets/Starter kit/TopDown2D/Scripts/EnemyBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJamStarterKit.TopDown2D
+{
+    [AddComponentMenu("TopDown2D/EnemyBase")]
+    [RequireComponent(typeof(AILerp), typeof(Collider2D))]
+    public class EnemyBase : MonoBehaviour
+    {
+        public int Health = 100;
+        public float Speed = 1;
+
+        // Use this for initialization
+        void Start()
+        {
+            GetComponent<AILerp>().speed = Speed;
+        }
+
+        public virtual void TakeDamage(int damage)
+        {
+            Health -= damage;
+            if (Health <= 1)
+                Die();
+        }
+
+        public virtual void Die()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Starter kit/TopDown2D/Scripts/EnemyChaser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameJamStarterKit.TopDown2D;
+
+public class EnemyChaser : EnemyBase {
+
+    public GameObject Body;
+
+    public override void Die()
+    {
+        Instantiate(Body, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
